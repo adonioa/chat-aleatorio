e9b97289947d05cf439833d7cc3435c7f1b51314
adonioa <adonioalves@gmail.com>
2026-01-15 01:27:48 +0000

    Atualização: chat sem vídeo, novo servidor e melhorias

--- server.js
+++ server.js
@@ -1,91 +1,70 @@
-// server.js - Chat Aleatório Avançado
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
-const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static(__dirname));
 
 let waitingUser = null;
 let onlineUsers = 0;
 
-// Lista de palavras proibidas
-const bannedWords = ['palavrão1', 'palavrão2', 'palavrão3'];
-
-io.on('connection', (socket) => {
+io.on("connection", socket => {
   onlineUsers++;
-  io.emit('updateOnline', onlineUsers);
 
-  console.log('Novo usuário conectado:', socket.id);
+  socket.on("set-nickname", nickname => {
+    socket.nickname = nickname || "Anônimo";
+    tryMatch(socket);
+    io.emit("online-count", onlineUsers);
+  });
 
-  socket.partner = null; // parceiro atual
+  socket.on("message", msg => {
+    if (socket.partner) {
+      socket.partner.emit("message", {
+        from: socket.nickname,
+        text: msg
+      });
+    }
+  });
 
-  // Função para conectar com outro usuário
-  const connectUser = () => {
-    if (waitingUser && waitingUser.id !== socket.id) {
-      const room = socket.id + '#' + waitingUser.id;
-      const partner = waitingUser;
-      socket.partner = partner;
-      partner.partner = socket;
+  socket.on("next", () => {
+    disconnectPair(socket);
+    tryMatch(socket);
+  });
 
-      socket.join(room);
-      partner.join(room);
+  socket.on("disconnect", () => {
+    onlineUsers--;
+    disconnectPair(socket);
+    io.emit("online-count", onlineUsers);
+  });
 
-      socket.emit('connected', 'Você foi conectado!');
-      partner.emit('connected', 'Você foi conectado!');
+  function tryMatch(user) {
+    if (waitingUser && waitingUser !== user) {
+      user.partner = waitingUser;
+      waitingUser.partner = user;
+
+      user.emit("system", "Conectado com um estranho");
+      waitingUser.emit("system", "Conectado com um estranho");
 
       waitingUser = null;
     } else {
-      waitingUser = socket;
-      socket.emit('connected', 'Esperando por outro usuário...');
+      waitingUser = user;
+      user.emit("system", "Procurando alguém...");
     }
-  };
+  }
 
-  connectUser();
-
-  // Receber mensagens
-  socket.on('chat message', (msg) => {
-    // Filtrar palavras proibidas
-    const filtered = msg.split(' ').map(word => bannedWords.includes(word.toLowerCase()) ? '***' : word).join(' ');
-    
-    if (socket.partner) {
-      socket.partner.emit('chat message', filtered);
+  function disconnectPair(user) {
+    if (user.partner) {
+      user.partner.emit("system", "O estranho saiu do chat");
+      user.partner.partner = null;
     }
-  });
-
-  // Botão “Próximo”
-  socket.on('next', () => {
-    // Avisar parceiro antigo
-    if (socket.partner) {
-      socket.partner.emit('chat message', 'O outro usuário saiu.');
-      socket.partner.partner = null;
-    }
-    socket.partner = null;
-
-    // Colocar o usuário de volta na fila
-    connectUser();
-  });
-
-  // Desconectar
-  socket.on('disconnect', () => {
-    onlineUsers--;
-    io.emit('updateOnline', onlineUsers);
-
-    if (socket.partner) {
-      socket.partner.emit('chat message', 'O outro usuário saiu.');
-      socket.partner.partner = null;
-    }
-
-    if (waitingUser && waitingUser.id === socket.id) {
-      waitingUser = null;
-    }
-  });
+    user.partner = null;
+  }
 });
 
 const PORT = process.env.PORT || 3000;
-
-http.listen(PORT, () => {
-  console.log('Servidor rodando na porta', PORT);
+server.listen(PORT, () => {
+  console.log("Servidor rodando na porta", PORT);
 });
-
